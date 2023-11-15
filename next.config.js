9c089934e78509df0d8f60223ce6bb728f1be6b3
oriol mazariegos <mazeitor@gmail.com>
2023-11-15 10:15:18 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,72 +1,22 @@
 /** @type {import('next').NextConfig} */
 const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
 
-// module.exports = {
-//     webpack(config, options) {
-//         if (!options.isServer) {
-//             config.plugins.push(
-//                 new NextFederationPlugin({
-//                     name: 'remote_manuscript',
-//                     filename: 'static/chunks/remoteEntry.js',
-//                     remoteType: "var",
-//                     exposes: {
-//                         './manuscript': './src/components/manuscript.tsx',
-//                     }
-//                 })
-//             );
-//         }
-//
-//         return config;
-//     },
-//     reactStrictMode: true, // Enable React Strict Mode
-// };
-
-//
-// module.exports = {
-//     webpack: (config, options) => {
-//         config.plugins.push(
-//             new options.webpack.container.ModuleFederationPlugin({
-//                 name:"remote_manuscript",
-//                 filename: "static/chunks/remoteEntry.js",
-//                 remoteType: "var",
-//                 exposes: {
-//                     "./manuscript": "./src/components/Manuscript.tsx"
-//                 },
-//                 shared: {
-//                     react: {
-//                         eager: true,
-//                         singleton: true
-//                     },
-//                     "react-dom": {
-//                         eager: true,
-//                         singleton: true
-//                     }
-//                 }
-//             })
-//         )
-//         return config
-//     },
-//     reactStrictMode: true,
-// };
-
-
 module.exports = {
     webpack(config, options) {
-        config.plugins.push(
-            new NextFederationPlugin({
-                name: 'remote_manuscript',
-                filename: 'static/chunks/remoteEntry.js',
-                exposes: {
-                    './manuscript': './src/components/Manuscript.tsx'
-                },
-                shared: {},
-                extraOptions:{
-                    exposePages: true
-                }
-            }),
-        );
+        if (!options.isServer) {
+            config.plugins.push(
+                new NextFederationPlugin({
+                    name: 'remote_manuscript',
+                    filename: 'static/chunks/remoteEntry.js',
+                    remoteType: "var",
+                    exposes: {
+                        './manuscript': './src/components/manuscript.tsx',
+                    }
+                })
+            );
+        }
 
         return config;
     },
-    reactStrictMode: true,
+    reactStrictMode: true, // Enable React Strict Mode
 };
