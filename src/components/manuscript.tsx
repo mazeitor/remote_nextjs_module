8481524cd7e4de1fbcc8bad5833938a8b60c461e
oriol mazariegos <mazeitor@gmail.com>
2023-11-15 10:08:14 +0000

    Add files via upload

---
+++ src/components/manuscript.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+// import styles from "./manuscript.module.css";
+import "tailwindcss/tailwind.css";
+import {Submission} from "@shared/interfaces/Submission";
+
+interface ManuscriptProps {
+    submission?: Submission;
+}
+
+function Manuscript(props: ManuscriptProps) {
+
+    return (
+        props.submission ?
+        <div>
+            <h2 className={"mt-8 text-3xl font-serif"}>{props.submission.title}</h2>
+            <div
+                className="mt-4 text-sm italic">{props.submission.authors
+                .map(author => `${author.firstName} ${author.lastName}`)
+                .join(', ')}
+            </div>
+            <h3 className={"mt-4 text-2xl font-serif"}>Abstract</h3>
+            <p className={"mt-2 font-serif"}>{props.submission.abstractText}</p>
+        </div> : null
+    )
+}
+
+export default Manuscript
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Manuscript from "@/components/manuscript";
+
+export default function Article() {
+    return (
+        <div>
+            Running in stand alone mode...
+            <Manuscript submission = {undefined} />
+        </div>
+    )
+}
